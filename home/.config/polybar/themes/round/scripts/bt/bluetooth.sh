fada22de662bf197e8bc1111bb1c8a2db7451c55
rototrash <siddharthrshete03@gmail.com>
2022-01-17 18:25:57 +0530

    chores: fix polybar

--- home/.config/polybar/themes/round/scripts/bt/bluetooth.sh
+++ home/.config/polybar/themes/round/scripts/bt/bluetooth.sh
@@ -1,13 +1,13 @@
-#!/bin/sh
+#!/bin/bash
 
 if [ $(bluetoothctl show | grep "Powered: yes" | wc -c) -eq 0 ]
 then
-  echo "  off "
+  echo " off"
 else
   if [ $(echo info | bluetoothctl | grep 'Device' | wc -c) -eq 0 ]
   then 
-    echo "  "
+    echo ""
   fi
-  echo "  "
+  echo ""
 fi
 
